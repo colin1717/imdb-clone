b160518e0ba4bf2ce57fb9ee9ffe47d1740f3e3f
Colin Hancock <colin1717@mac.com>
2016-05-18 15:36:03 -0500

    actors working

---
+++ app/controllers/actors_controller.rb
@@ -0,0 +1,37 @@
+class ActorsController < ApplicationController
+  before_action :set_actor, only: [:show, :edit, :update]
+
+  def index
+    @actors = Actor.all
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def new
+    @actor = Actor.new
+  end
+
+  def create
+    @actor = Actor.new(actor_params)
+    if @actor.save
+      redirect_to @actor
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def set_actor
+    @actor = Actor.find(params[:id])
+  end
+
+  def actor_params
+    params.require(:actor).permit(:name, :year_of_birth, :thumbnail)
+  end
+
+end
